f35af269bd27721b59910c2f584b38a48828ab52
yjcho <jinjanic91@gmail.com>
2022-05-18 21:16:47 +0900

    feat. lint설정 및 기본 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
+  // app 컴포넌트를 렌더링합니다.
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용하여 원하는 엘리먼트에 접근을 한다. (ID로 접근)
+  const counterEl = screen.getByTestId('counter');
+  // id가 counter인 엘리먼트의 텍스트가 0인지 확인
+  expect(counterEl).toHaveTextContent(0);
 });
+
+test('minus Btn has correct text "minus"', () => {
+  render(<App />);
+  const minusBtn = screen.getByTestId('minusBtn');
+  expect(minusBtn).toHaveTextContent('-');
+});
+
+test('plus Btn has correct text "plus"', () => {
+  render(<App />);
+  const plusBtn = screen.getByTestId('plusBtn');
+  expect(plusBtn).toHaveTextContent('+');
+});
+
+// 기본적으로 되어있는 구조.
+// test('renders learn react link', () => {
+//   render(<App />);
+//   // const linkElement = screen.getByText(/learn react/i);
+//   // expect(linkElement).toBeInTheDocument();
+
+//   // linst 설정 제대로 되었는지 테스트
+//   // const lintTest = screen.getByRole('button', { name: 'lintTest' });
+//   // expect(lintTest).toHaveTextContent('lintTest');
+// });
